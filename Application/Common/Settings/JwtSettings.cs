fc0c66d2cfb49d003bd54760f1abfb8f2c19b390
Vansh Yelekar <vanshyelekar04@gmail.com>
2025-06-17 09:45:34 +0530

    Add Common folder with Settings.cs

---
+++ Application/Common/Settings/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Settings;
+
+public class JwtSettings
+{
+    public string Key { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+}
